8d39be7ad2d3482fcc74f2301244e5bf3c08ccef
Allan Dutra Feitosa <allandutra@radinfo.com.br>
2023-01-05 14:48:09 -0300

    feat: class application layer pt6

---
+++ DevFreela.Application/Services/Implementations/UserService.cs
@@ -0,0 +1,38 @@
+using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        public UserService(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int Create(CreateUserInputModel inputModel)
+        {
+            var user = new User(inputModel.FullName, inputModel.Email, inputModel.BirthDate);
+
+            _dbContext.Users.Add(user);
+
+            return user.Id;
+        }
+
+        public UserViewModel GetUser(int id)
+        {
+            var user = _dbContext.Users.SingleOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return new UserViewModel(user.FullName, user.Email);
+        }
+    }
+}
