5848bdda7dce380c10dd4177710969614a782285
AllanDutra <allandutrafeitosa14@gmail.com>
2023-01-15 13:56:37 -0300

    feat: writing first unit test

---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,30 @@
+using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestIfProjectStartWorks()
+        {
+            var project = new Project("Teste", "Descrição Teste", 1, 2, 10000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            // ? {VALOR ESPERADO}, {VALOR REAL}
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
