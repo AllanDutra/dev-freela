efe1882c0725ee99145bad9732d34f98a71eef08
AllanDutra <allandutrafeitosa14@gmail.com>
2023-01-19 22:50:37 -0300

    refactor: status payment pending

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -50,7 +50,7 @@ namespace DevFreela.Core.Entities
 
         public void Finish()
         {
-            if (Status == ProjectStatusEnum.InProgress)
+            if (Status == ProjectStatusEnum.PaymentPending)
             {
                 Status = ProjectStatusEnum.Finished;
                 FinishedAt = DateTime.Now;
