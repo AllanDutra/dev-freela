0c56a3236e97b823a0b6562f060eeec5ccccb9be
AllanDutra <allandutrafeitosa14@gmail.com>
2023-01-31 18:10:44 -0300

    feat: fixing project query unit test

--- DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
+++ DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -1,5 +1,6 @@
 using DevFreela.Application.Queries.GetAllProjects;
 using DevFreela.Core.Entities;
+using DevFreela.Core.Models;
 using DevFreela.Core.Repositories;
 using Moq;
 
@@ -13,29 +14,32 @@ namespace DevFreela.UnitTests.Application.Queries
         {
             // ? PADRÃO AAA
             // ? Arrange
-            var projects = new List<Project>
+            var projects = new PaginationResult<Project>
             {
-                new Project("Nome do Teste 1", "Descrição De Teste 1", 1, 2, 10000),
-                new Project("Nome do Teste 2", "Descrição De Teste 2", 1, 2, 20000),
-                new Project("Nome do Teste 3", "Descrição De Teste 3", 1, 2, 30000),
+                Data = new List<Project>
+                {
+                    new Project("Nome do Teste 1", "Descrição De Teste 1", 1, 2, 10000),
+                    new Project("Nome do Teste 2", "Descrição De Teste 2", 1, 2, 20000),
+                    new Project("Nome do Teste 3", "Descrição De Teste 3", 1, 2, 30000)
+                }
             };
 
             // * Criando o moq
             var projectRepositoryMock = new Mock<IProjectRepository>();
-            projectRepositoryMock.Setup(pr => pr.GetAllAsync().Result).Returns(projects);
+            projectRepositoryMock.Setup(pr => pr.GetAllAsync(It.IsAny<string>(), It.IsAny<int>()).Result).Returns(projects);
 
-            var getAllProjectsQuery = new GetAllProjectsQuery("");
+            var getAllProjectsQuery = new GetAllProjectsQuery { Query = "", Page = 1 };
             var getAllProjectsQueryHandler = new GetAllProjectsQueryHandler(projectRepositoryMock.Object);
 
             // ? Act
-            var projectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new CancellationToken());
+            var paginationProjectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new CancellationToken());
 
             // ? Assert
-            Assert.NotNull(projectViewModelList);
-            Assert.NotEmpty(projectViewModelList);
-            Assert.Equal(projects.Count, projectViewModelList.Count);
+            Assert.NotNull(paginationProjectViewModelList);
+            Assert.NotEmpty(paginationProjectViewModelList.Data);
+            Assert.Equal(projects.Data.Count, paginationProjectViewModelList.Data.Count);
 
-            projectRepositoryMock.Verify(pr => pr.GetAllAsync().Result, Times.Once);
+            projectRepositoryMock.Verify(pr => pr.GetAllAsync(It.IsAny<string>(), It.IsAny<int>()).Result, Times.Once);
         }
     }
 }
